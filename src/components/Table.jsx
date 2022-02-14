e32eb27cbb403a898ea45b6ce67226feaff8402a
Renato Moreno <renats.mc@gmail.com>
2022-02-14 17:30:21 -0300

    ajuste scroll lateral

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -15,10 +15,10 @@ class Table extends React.Component {
   render() {
     const { expenses, deleteExpense } = this.props;
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col teste">
         {/* como criar uma tabela ref: https://blog.betrybe.com/html/table-html/ */}
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div className="-my-2 overflow-x-auto sm:-mx-4 lg:-mx-0">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-4 lg:px-4">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200 table  text-center">
                 <thead className="bg-gray-50">
